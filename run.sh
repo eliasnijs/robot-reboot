0898677d69bf261e71caa0db32618a01f68bbb7e
Elias Nijs <elias.nijs@gmail.com>
2023-05-07 21:42:16 +0200

    draft verslag

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 clear
-swipl -t halt -q -s src/main.pl -- --gen=[2,4,4]
+
+swipl -q -t halt -s src/main.pl -- --game=[resources/lv2.txt]
 
 # cat 'resources/lv1.txt' | time swipl -q src/main.pl -- --solve
 # cat 'resources/lv2.txt' | time swipl -q -s src/main.pl -- --solve
-# swipl -q -t halt -s src/main.pl -- --game=[resources/lv2.txt]
+
+# swipl -t halt -q -s src/main.pl -- --gen=[2,4,4]
+
+# cat 'resources/lv1.txt' | swipl -q src/main.pl -- --test=[0,\'U\']
+
