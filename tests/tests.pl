63c2064b3a3837bf15ded6ae2b9b06c84f5775ec
Elias Nijs <elias.nijs@gmail.com>
2023-05-08 13:34:45 +0200

    cleanup

--- tests/tests.pl
+++ tests/tests.pl
@@ -10,7 +10,7 @@ lv(0, "┏━━━━━┓
 "
 ).
 
-lv(0, "
+lv(1, "
 ┏━┓
 ┃▣┃
 ┗━┛
@@ -41,22 +41,22 @@ test(move_left_1, [nondet]) :-
 	\+ ( P0 = P1).
 
 test(move_down_2, [nondet]) :-
-	lv(0, S),
+	lv(1, S),
 	parse(S, P0),
 	move(P0, 0, vec2(0,1), P1),
 	P0 = P1.
 test(move_up_2, [nondet]) :-
-	lv(0, S),
+	lv(1, S),
 	parse(S, P0),
 	move(P0, 0, vec2(0,-1), P1),
 	P0 = P1.
 test(move_right_2, [nondet]) :-
-	lv(0, S),
+	lv(1, S),
 	parse(S, P0),
 	move(P0, 0, vec2(1,0), P1),
 	P0 = P1.
 test(move_left_2, [nondet]) :-
-	lv(0, S),
+	lv(1, S),
 	parse(S, P0),
 	move(P0, 0, vec2(-1,0), P1),
 	P0 = P1.
